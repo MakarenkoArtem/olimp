8362ab5e0c1b9ce1ef5703934620b3dfb2e13d41
Артем <makarenkoiandeks@yandex.ru>
2021-10-03 20:41:26 +0400

    696 не работает

--- olimp1.py
+++ olimp1.py
@@ -1,16 +1,8 @@
 with open("input.txt", "r") as file:
     data = [i.rstrip("\n") for i in file.readlines()]
 with open("output.txt", "w") as file:
-    k, s, _ = [int(i) for i in data[0].split()]
-    p = [int(i) for i in data[1].split()]
-    d = 1
-    while 1:
-        if d == s and k:
-            if d % 7 != 6 and d % 7 != 0 and d not in p:
-                k -= 1
-            s += 1
-        elif not k:
-            if d % 7 != 6 and d % 7 != 0 and d not in p:
-                break
-        d += 1
-    file.write(f"{d}")
+    t, s, x, y = [int(i) for i in data[0].split()]
+    t -= 10
+    if t < 0:
+        t = 0
+    file.write(str(s * y + x * t))
